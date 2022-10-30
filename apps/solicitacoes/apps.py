ff58b499e6883d2257aab396268638531be259da
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-10-29 23:08:34 -0300

    Iniciada app solicitacoes

---
+++ apps/solicitacoes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SolicitacoesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.solicitacoes'
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'accounts',
     'apps.core',
     'apps.locais',
+    'apps.solicitacoes',
 
 ]
 
