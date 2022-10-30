0b9026fe6c35710adfb4ce44c38c25f75169fa72
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-10-29 22:40:37 -0300

    Implementado model Endereco

--- apps/locais/models.py
+++ apps/locais/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from apps.core.models import Auditoria
 
-# Create your models here.
+
+class Endereco(Auditoria):
+    logradouro = models.CharField(max_length=150, null=True, blank=True, verbose_name='Logradouro')
+    bairro = models.CharField(max_length=50, null=True, blank=True, verbose_name='Bairro')
+    cep = models.CharField(max_length=8, null=True, blank=True, verbose_name='CEP')
+
+    def __str__(self):
+        return str(self.logradouro) + ', ' + str(self.bairro)
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
+        ordering = ['logradouro', 'bairro']
