0a151fab57d8b356c0708f2e67f4997a0534d235
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-10-30 17:36:48 -0300

    Realizados ajustes nos parâmetros da classe OrdemServicoHistoryAdmin

--- apps/ordens/admin.py
+++ apps/ordens/admin.py
@@ -5,6 +5,19 @@ from simple_history.admin import SimpleHistoryAdmin
 
 class OrdemServicoHistoryAdmin(SimpleHistoryAdmin):
     filter_horizontal = ('solicitacoes', )
+    list_display = ['id', 'status_servico', 'data_execucao', 'detalhe_servico', ]
+    search_fields = ['id', 'observacao', 'detalhe_servico', ]
+    list_filter = ['status_servico', 'data_execucao', ]
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('solicitacoes', ),
+                ('detalhe_servico',),
+                ('status_servico', 'data_execucao', ),
+                ('observacao', ),
+            )
+        }),
+    )
 
 
 admin.site.register(OrdemServico, OrdemServicoHistoryAdmin)
