73940104da7e8edc73a47daf94d70482efe6e0e7
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-10-30 11:44:15 -0300

    Registrado model OrdemServico no módulo admin

--- apps/ordens/admin.py
+++ apps/ordens/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import OrdemServico
+from simple_history.admin import SimpleHistoryAdmin
 
-# Register your models here.
+
+class OrdemServicoHistoryAdmin(SimpleHistoryAdmin):
+    filter_horizontal = ('solicitacoes', )
+
+
+admin.site.register(OrdemServico, OrdemServicoHistoryAdmin)
